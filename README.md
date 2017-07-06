7fe69611397d538dd77f3fb08951ee753d70abed
Jeane Carlos <volbohel@gmail.com>
2017-07-06 13:40:53 -0400

    Second Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Mobile App Documentation Will Go Here
+
+jeane@ionic6.org
+i6pass
+
+georgia@ionic6.org
---
+++ ionic6/LoginViewController.swift
@@ -0,0 +1,58 @@
+//
+//  LoginViewController.swift
+//  ionic6
+//
+//  Created by Jeane Carlos on 7/5/17.
+//  Copyright © 2017 ionic6. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet var mainView: UIView!
+    
+    var mainColor: UIColor!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mainColor = mainView.backgroundColor
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func signinPressed(_ sender: UIButton) {
+        
+        //Data validation here
+        if let email = usernameField.text, let pass = passwordField.text {
+            
+            Auth.auth().signIn(withEmail: email, password: pass) { (user, error) in
+                if let u = user {
+                    print(u)
+                    self.performSegue(withIdentifier: "login", sender: self)
+                } else {
+                    self.mainView.backgroundColor = UIColor.brown
+                    
+                }
+            }
+            
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.mainView.backgroundColor = self.mainColor
+        usernameField.resignFirstResponder()
+        passwordField.resignFirstResponder()
+        
+    }
+    
+}
+
+
